ff544602325ac7758099bb77b38d941c64defd9d
Star1122 <kitdb@outlook.com>
2018-07-04 16:59:39 +0200

    Update Transactions model

--- models/Transactions.js
+++ models/Transactions.js
@@ -2,15 +2,16 @@ const mongoose = require('mongoose');
 
 const transactionsSchema = new mongoose.Schema({
     orderId: String,
-    coinId: String,
-    pair: String,
-    side: String,
-    txnHash: String,
-    quantity: { type: Number, default: 0.0 },
-    amount: { type: Number, default: 0.0 },
-    type: String,
-    smallestUnit: String,
-    status: String
+    blockHash: String,
+    blockNumber: Number,
+    contractAddress: String,
+    cumulativeGasUsed: Number,
+    gasUsed: Number,
+    from: String,
+    to: String,
+    status: String,
+    transactionHash: String,
+    transactionIndex: Number
 }, { timestamps: true });
 
 const Transactions = mongoose.model('Transactions', transactionsSchema);
