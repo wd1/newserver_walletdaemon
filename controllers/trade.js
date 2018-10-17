8e23c39d2def2a7fada7df8a6c41502dfbe98362
Star1122 <kitdb@outlook.com>
2018-10-17 16:33:13 +0200

    Remove order validation in order watcher

--- controllers/trade.js
+++ controllers/trade.js
@@ -655,15 +655,15 @@ const eventsManager = async () => {
                                 console.log('Start order: ', order._id);
 
                                 try {
-                                    if (new Date().getTime() - new Date(order.updatedAt).getTime() > 86400000) {
-                                        order.status = 'Failed';
-                                        order.save(err => {
-                                            if (err) {
-                                                console.log('eventsManager: order.save: ', err);
-                                            }
-                                        });
-                                        return;
-                                    }
+                                    // if (new Date().getTime() - new Date(order.updatedAt).getTime() > 86400000) {
+                                    //     order.status = 'Failed';
+                                    //     order.save(err => {
+                                    //         if (err) {
+                                    //             console.log('eventsManager: order.save: ', err);
+                                    //         }
+                                    //     });
+                                    //     return;
+                                    // }
 
                                     const ords = await Orders.find({ txId: { $ne: null } }, 'txId', { lean: true }).exec();
                                     for (let i = 0; i < events.length; i++) {
