dcc769056be05191c7b999cdbf5deb9fef8922f5
Byron Levels <byron@coinve.st>
2019-04-19 13:11:25 -0500

    Update for merge conflicts with CWT-655

--- controllers/coin.js
+++ controllers/coin.js
@@ -1,8 +1,7 @@
 import rp from 'request-promise';
 import redisClient from '../redis';
 import Coins from '../models/Coins';
-import { cryptoIdToSymbolAll, CMC_API_URL, CMC_API_SECRET, CC_API_URL, CC_API_KEY } from '../services/Config';
-import { cryptoIdToSymbol, CMC_API_URL, CMC_API_SECRET, CC_API_URL, CC_API_KEY, PRICE_API_URL } from '../services/Config';
+import { cryptoIdToSymbolAll, CMC_API_URL, CMC_API_SECRET, CC_API_URL, CC_API_KEY, PRICE_API_URL } from '../services/Config';
 
 export const fetchCoinPrices = async () => {
     console.log(`\n------------- Fetching Token Prices from CoinMarketCap ------------`);
