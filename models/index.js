5c3a8816050de17cd5bd306a49dbc7622aaa88d2
Byron Levels <byron@coinve.st>
2019-04-27 14:50:24 -0500

    Added Winston logging. Added note for deprecated warnings.

--- models/index.js
+++ models/index.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose');
 const chalk = require('chalk');
+const { logger } = require('../services/logger');
 
 exports.initializeDB = () => {
     /**
      * Connect to MongoDB.
      */
+    // These options cause the following to fail on first attempt:
+    // const order = await Orders.findOne({ status: 'Open', inputHash: hash, 'receipt.transactionHash': event.transactionHash }).exec();
+    // mongoose.set('useNewUrlParser', true);
+    // mongoose.set('useCreateIndex', true);
     mongoose.connect(process.env.MONGODB_URI);
     mongoose.connection.on('error', err => {
-        console.error(err);
-        console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
-
+        logger.log('error', { label: 'MongoConnection', message: `MongoDB connection error. Please make sure MongoDB is running.: ${err}` });
         process.exit();
     });
 };
